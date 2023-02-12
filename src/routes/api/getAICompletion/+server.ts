b0f84910b21acaa9d132f2616fb78ecd3bc061ea
CarsonWebster <CarsonWebster@hotmail.com>
2023-02-12 13:06:36 -0800

    Some futher error handling

--- src/routes/api/getAICompletion/+server.ts
+++ src/routes/api/getAICompletion/+server.ts
@@ -6,13 +6,16 @@ const configuration = new Configuration({
   });
 
 export async function GET() {
+    console.log("getAICompletion call!")
     const openai = new OpenAIApi(configuration);
+    console.log("Openai created!")
     const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: "Say this is a test",
     max_tokens: 7,
     temperature: 0,
     });
+    console.log("Got the response!")
     console.log(response.data.choices[0].text)
     return json(response.data.choices[0].text)
 }
--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,13 +1,30 @@
 <script lang='ts'>
     let completion = "";
+	let completion_loading = false;
+	let error = false;
+
 	async function getCompletion() {
+		completion_loading = true;
 		const response = await fetch('/api/getAICompletion');
-		completion = await response.json();
-        console.log(completion);
+		if (!response.ok) {
+			error = true;
+		} else {
+			completion = await response.json();
+			console.log(completion);
+			error = false;
+		}
+		completion_loading = false;
 	}
 </script>
 <h1>Info page</h1>
 <button on:click={getCompletion}>Get Completion</button>
-{#if completion !== ""}
-	<p>{completion}</p>
+{#if completion_loading}
+	<p>Loading...</p>
+{:else}
+	{#if error}
+		<p>An error has occurred. Make sure you have set your API key with `export OPENAI_API_KEY='key'`</p>
+	{/if}
+	{#if completion !== ""}
+		<p>{JSON.stringify(completion)}</p>
+	{/if}
 {/if}
