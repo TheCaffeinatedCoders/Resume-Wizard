30740a40585a8260655987a7e3544cf48300cd71
CarsonWebster <CarsonWebster@hotmail.com>
2023-02-13 18:54:09 -0800

    Fixed up Calvins test-lodash function to work inTS

---
+++ src/routes/api/test-lodash/+server.ts
@@ -0,0 +1,17 @@
+// Import lodash
+import _ from 'lodash';
+import { readFile } from 'fs/promises';
+
+export async function GET() {
+	console.log('test-lodash function');
+	try {
+		const latexTemplate = await readFile('$lib/templates/test_template.tex', 'utf8');
+		let variableMap = { name: 'Calvin', phone: '4259855538' };
+		_.templateSettings.interpolate = /\@{([\s\S]+?)}/g;
+		let compiled = _.template(latexTemplate);
+		let compiledTemplate = compiled(variableMap);
+		console.log(compiledTemplate);
+	} catch (error) {
+		console.error(error);
+	}
+}
