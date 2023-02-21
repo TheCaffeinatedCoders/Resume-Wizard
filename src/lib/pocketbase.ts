2d092502180394457808a83a894efe7b25c1ef14
Carson Webster <carsonwebster.com>
2023-02-21 02:50:44 +0000

    Switched pocketbase url from linode server to domain name

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -6,7 +6,7 @@ import { writable } from 'svelte/store';
 // Feel free to vist this link to see the database
 // http://45.56.90.168/_/
 // Ping Carson on discord if you need an account set up to view the database
-export const pb = new PocketBase('http://45.56.90.168:80');
+export const pb = new PocketBase('http://ResumeWizard.tech');
 
 // Current user variable is equal to a writable store, a svelte way of storing data
 // pb.authStore.model is the default object, which is equal to NULL when the user is not logged in
@@ -16,6 +16,6 @@ export const currentUser = writable(pb.authStore.model)
 // Create a callback function that will run when the authStore changes
 // This will update the currentUser variable with the new authStore model
 pb.authStore.onChange((auth) => {
-    console.log('authStore changed', auth);
-    currentUser.set(pb.authStore.model)
+  console.log('authStore changed', auth);
+  currentUser.set(pb.authStore.model)
 })
