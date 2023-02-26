539d0ea9822ef248046fd07650ff68fe6039f571
CarsonWebster <CarsonWebster@hotmail.com>
2023-02-25 17:59:56 -0800

    Added some new routes to add code into

---
+++ src/routes/edit/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Edit profile page</h1>
+<a href="/home"> <button>Home</button> </a>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { pb } from '$lib/pocketbase';
+	function signOut() {
+		pb.authStore.clear();
+	}
+</script>
+<h1>Home page</h1>
+<!-- Button to navigate to edit profile page -->
+<a href="/edit"> <button>Edit profile</button> </a>
+<button on:click={signOut}>Logout</button>
