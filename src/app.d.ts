6d993395b4d3cb431a8944fb08059697802fd825
CarsonWebster <CarsonWebster@hotmail.com>
2023-05-05 17:07:49 -0700

    Custom typed objects

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,3 +7,57 @@ declare namespace App {
 	// interface PageData {}
 	// interface Platform {}
 }
+
+declare class PersonalInfoObject {
+	firstName: string
+	lastName: string
+	phoneNumber: string
+	email: string
+	address: string
+	city: string
+	state: string
+	zipCode: string
+	github: string
+	linkedin: string
+}
+
+declare class EducationObject {
+	school: string
+	degree: string
+	startDate: Date
+	endDate: Date
+	currentlyAttending: boolean
+	description: string
+}
+
+declare class JobObject {
+	company: string
+	position: string
+	startDate: Date
+	endDate: Date
+	currentlyWorking: boolean
+	description: string
+}
+
+declare class ProjectObject {
+	name: string
+	startDate: Date
+	endDate: Date
+	description: string
+	technologies: string[]
+}
+
+declare class SkillsObject {
+	languages: string[]
+	frameworks: string[]
+	tools: string[]
+	libaries: string[]
+}
+
+declare class ResumeObject {
+	personalInfo: PersonalInfoObject
+	education: EducationObject[]
+	jobs: JobObject[]
+	projects: ProjectObject[]
+	skills: SkillsObject
+}
