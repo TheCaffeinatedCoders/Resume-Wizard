dda10c78de364e9ea21074163b50276542cc28a6
CarsonWebster <CarsonWebster@hotmail.com>
2023-05-05 17:44:48 -0700

    TS file exporting resumeStore

---
+++ src/lib/resumeStore.ts
@@ -0,0 +1,19 @@
+import { localStorageStore } from '@skeletonlabs/skeleton';
+import type { Writable } from 'svelte/store';
+import { get } from 'svelte/store';
+import { currentUser } from './pocketbase';
+
+if (!currentUser) {
+  throw new Error('currentUser is not defined');
+}
+
+let user_id = get(currentUser)?.id;
+
+if (!user_id) {
+  user_id = 'INVALID_USER_ID';
+}
+
+// Define a key name for the store that includes the user ID
+const keyName = 'resumeObjects-${user_id}';
+// Create a writable store that will be saved to local storage
+export const resumeStore: Writable<ResumeObject[]> = localStorageStore(keyName, []);
