e22eb81cb4c471498b009859aed9ea42b01e8927
CarsonWebster <CarsonWebster@hotmail.com>
2023-05-15 15:12:28 -0700

    Login form now binds to values. Login with email works

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -104,6 +104,7 @@
 							required
 							class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
 							placeholder="Enter your email address"
+							bind:value={email}
 						/>
 					</div>
 				</div>
@@ -119,6 +120,7 @@
 							required
 							class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
 							placeholder="Enter your password"
+							bind:value={password}
 						/>
 					</div>
 				</div>
